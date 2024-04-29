b54ba3246b0c5c2dc2f7b3c34028ea7df621bb08
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-04-29 10:11:20 +0200

    Update README

---
+++ index.js
@@ -0,0 +1,43 @@
+const { promises: fs } = require('fs');
+const readme = require('./readme');
+
+const today = new Date();
+
+function generateNewREADME() {
+  const readmeRow = readme.split('\n');
+
+  function updateIdentifier(identifier, replaceText) {
+    const identifierIndex = findIdentifierIndex(readmeRow, identifier);
+    if (!readmeRow[identifierIndex]) return;
+    readmeRow[identifierIndex] = readmeRow[identifierIndex].replace(
+      `<#${identifier}>`,
+      replaceText
+    );
+  }
+
+  const identifierToUpdate = {
+    today_date: getTodayDate(),
+  };
+
+  Object.entries(identifierToUpdate).forEach(([key, value]) => {
+    updateIdentifier(key, value);
+  });
+
+  return readmeRow.join('\n');
+}
+
+function getTodayDate() {
+  return today.toDateString();
+}
+
+const findIdentifierIndex = (rows, identifier) =>
+  rows.findIndex((r) => Boolean(r.match(new RegExp(`<#${identifier}>`, 'i'))));
+
+const updateREADMEFile = (text) => fs.writeFile('./README.md', text);
+
+function main() {
+  const newREADME = generateNewREADME();
+  console.log(newREADME);
+  updateREADMEFile(newREADME);
+}
+main();
--- readme.js
+++ readme.js
@@ -1,4 +1,4 @@
-<h1 align="center"><b>Hi , I'm Shad Azuelos </b><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35"></h1>
+module.exports = `<h1 align="center"><b>Hi , I'm Shad Azuelos </b><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35"></h1>
 
 <p align="center">
   <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Second+Year+Computer+Science+Student,;Self-taught+Front-End+Developer,;Active+Learner/Researcher,;Love+to+learn+new+stuffs"></a>
@@ -121,3 +121,6 @@
 <br>
 <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
 <br>
+
+Last update on <#today_date>
+`;
