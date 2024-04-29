26c9376132adb1db292041b4310cfc210f0b4d7a
as105237 <shad.azuelos@etu.univ-cotedazur.fr>
2024-04-29 10:33:26 +0200

    Update README

--- readme.js
+++ readme.js
@@ -1,14 +1,14 @@
 module.exports = `<h1 align="center"><b>Hi , I'm Shad Azuelos </b><img src="https://media.giphy.com/media/hvRJCLFzcasrR4ia7z/giphy.gif" width="35"></h1>
 
 <p align="center">
-  <a href="https://github.com/DenverCoder1/readme-typing-svg"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Second+Year+Computer+Science+Student,;Self-taught+Front-End+Developer,;Active+Learner/Researcher,;Love+to+learn+new+stuffs"></a>
+  <a href="https://github.com/shadazls"><img src="https://readme-typing-svg.herokuapp.com?font=Time+New+Roman&color=cyan&size=25&center=true&vCenter=true&width=600&height=100&lines=Second+Year+Computer+Science+Student,;Self-taught+Front-End+Developer,;Active+Learner/Researcher,;Love+to+learn+new+stuffs"></a>
 </p>
 
 <br>
 
-## <picture><img src = "https://github.com/0xAbdulKhalid/0xAbdulKhalid/raw/main/assets/mdImages/about_me.gif" width = 50px></picture> **About me**
+## <picture><img src = "https://github.com/shadazls/shadazls/images/about_me.gif" width = 50px></picture> **About me**
 
-<picture> <img align="right" src="https://github.com/7oSkaaa/7oSkaaa/blob/main/Images/Right_Side.gif?raw=true" width = 250px></picture>
+<picture> <img align="right" src="https://github.com/shadazls/shadazls/images/Right_Side.gif?raw=true" width = 250px></picture>
 
 <br>
 
@@ -21,7 +21,7 @@ module.exports = `<h1 align="center"><b>Hi , I'm Shad Azuelos </b><img src="http
 
 <br><br>
 
-<img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif"><br><br>
+<img src="https://github.com/shadazls/shadazls/images/line.gif"><br><br>
 
 ## <img src="https://media2.giphy.com/media/QssGEmpkyEOhBCb7e1/giphy.gif?cid=ecf05e47a0n3gi1bfqntqmob8g9aid1oyj2wr3ds3mg700bl&rid=giphy.gif" width ="25"><b> Skills</b>
 <br>
